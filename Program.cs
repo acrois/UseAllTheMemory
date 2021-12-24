06accf59c6c104cdbd334bee65773dedaafe33d8
Aaron Croissette <sysfly@gmail.com>
2021-12-24 02:20:13 -0500

    Simply put: Use a bunch of memory.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace UseAllTheMemory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PerformanceCounter counter = new PerformanceCounter("Memory", "Available MBytes", null);
+            LinkedList<int> memstore = new LinkedList<int>();
+
+            while (true)
+            {
+                memstore.AddLast(1337);
+
+                if (memstore.Count % 1000000 == 0)
+                {
+                    Console.WriteLine(counter.NextValue());
+                }
+            }
+        }
+    }
+}
